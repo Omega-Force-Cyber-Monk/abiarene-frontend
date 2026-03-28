1aa05a3c276309f4991ac2e0bab66b95e164b268
imranmia <imran32472@gmail.com>
2026-03-28 09:03:08 +0600

    refactor: simplify ServerLayout and enhance ServerDashboardPage with table data

---
+++ src/pages/Server/DammyData.ts
@@ -0,0 +1,16 @@
+import { TableCardProps } from "./components/TableCard";
+
+export const tableData: TableCardProps[] = [
+  { id: 1, capacity: 4, status: 'OCCUPIED', subStatus: 'SERVED' },
+  { id: 2, capacity: 4, status: 'AVAILABLE' },
+  { id: 3, capacity: 4, status: 'OCCUPIED', subStatus: 'SERVED' },
+  { id: 4, capacity: 4, status: 'OCCUPIED', subStatus: 'SERVED' },
+  { id: 5, capacity: 2, status: 'OCCUPIED', subStatus: 'SERVED' },
+  { id: 6, capacity: 2, status: 'AVAILABLE' },
+  { id: 7, capacity: 4, status: 'AVAILABLE' },
+  { id: 8, capacity: 4, status: 'AVAILABLE' },
+  { id: 9, capacity: 4, status: 'AVAILABLE' },
+  { id: 10, capacity: 4, status: 'AVAILABLE' },
+  { id: 11, capacity: 2, status: 'AVAILABLE' },
+  { id: 12, capacity: 2, status: 'AVAILABLE' },
+];
--- src/pages/Server/ServerDashboardPage.tsx
+++ src/pages/Server/ServerDashboardPage.tsx
@@ -1,7 +1,30 @@
+import { TableCard } from "./components/TableCard";
+import { tableData } from "./DammyData";
+
 const ServerDashboardPage = () => {
   return (
-    <div>
-      <h1>ServerDashboardPage</h1>
+    <div className="pb-6">
+     <header className="mb-8 space-y-1.5">
+        <h1 className="text-3xl font-bold text-slate-800">Table Map</h1>
+        <p className="text-gray-500 font-semibold tracking-wide text-sm uppercase">
+          Select a table to start an order
+        </p>
+      </header>
+      <div>
+        {
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {tableData.map((table) => (
+          <TableCard 
+            key={table.id}
+            id={table.id}
+            capacity={table.capacity}
+            status={table.status}
+            subStatus={table.subStatus}
+          />
+        ))}
+      </div>
+        }
+      </div>
     </div>
   );
 };
